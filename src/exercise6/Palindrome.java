5f2a6c0cfe4b1c47d064467da0657049285859f9
Bordo30 <boris.dobrev30@gmail.com>
2021-11-18 13:56:17 +0200

    Preaaaaaaaty easy!

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int nextDigit;
+        int sum = 0;
+        boolean palindrome = false;
+        int temp = number;
+
+        while(number > 0) {
+            nextDigit = number % 10;
+            sum = (sum*10) + nextDigit;
+            number /= 10;
+        }
+
+        if(temp == sum) {
+            palindrome = true;
+        }
+
+        System.out.println("Palindrome: " + palindrome);
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -4,6 +4,35 @@ import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter password: ");
+        boolean valid = false;
+
+        do {
+            String password = input.nextLine();
+
+            if (password.length() >= 8) {
+                valid = true;
+
+                for (int i = 0; i < password.length(); i++) {
+
+                    if (Character.isLetterOrDigit(password.charAt(i))) {
+                        valid = true;
+
+                        int counter = 0;
+
+                        if (Character.isDigit(password.charAt(i))) {
+                            counter++;
+                            if (counter >= 2) {
+                                valid = true;
+                            }
+                        }
+                    }
+                }
+            }
+        } while(valid == false);
+
+        System.out.println("Password is valid!");
     }
 }
